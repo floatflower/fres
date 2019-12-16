3a0d32cab104ad52a89a3c778556f3b478deb310
FloatFlower.Huang <floatflower@fresco.tech>
2019-12-16 22:15:31 +0800

    Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ services.set('knex', require('./knex'));
 services.set('event-handler.loader', require('./event-handler-loader'));
 services.set('response.handler', require('./response-handler'));
 services.set('repository.loader', require('./repository-loader'));
+services.set('view.data.provider.loader', require('./view-data-provider-loader'));
 services.set('data.handler', require('./data-handler'));
 services.set('validation', require('./validation'));
 services.set('http', require('./http'));
